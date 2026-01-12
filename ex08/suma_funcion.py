d259212490900ed0151823c6a4630d57db1dc905
marcos <marcoszabalarodriguez@gmail.com>
2026-01-12 13:27:57 +0100

    feat: Added ex08

---
+++ ex08/suma_funcion.py
@@ -0,0 +1,6 @@
+def sumar(a,b):
+    return a + b
+num1 = float(input("Ingrese el primer número: "))
+num2 = float(input("Ingrese el segundo número: "))  
+resultado = sumar(num1, num2)
+print(f"La suma de {num1} y {num2} es {resultado}")
