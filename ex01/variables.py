ef79390ffb448a71b26cb88bb47d8afb92b59bfd
marcos <marcoszabalarodriguez@gmail.com>
2026-01-12 12:43:11 +0100

    feat: Added ex02

--- ex01/variables.py
+++ ex01/variables.py
@@ -1,3 +1,5 @@
 x = 10
 y = 10.5
 z = "Hello, World!"
+
+print(type(x), type(y), type(z))
---
+++ ex02/operaciones_basicas.py
@@ -0,0 +1,5 @@
+x = 5
+y = 2
+sum_result = x + y
+f_string_result = f"The sum of {x} and {y} is {sum_result}"
+print(f_string_result)
